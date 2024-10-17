3e144c4e00527609cbde5f2d380c5ec1ff399e49
SergioGMR <sergiogmr@icloud.com>
2024-10-17 21:04:44 +0100

    Hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
+  output: 'hybrid',
 
   env: {
     schema: {
