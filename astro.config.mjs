e681fa085c98e5f1d25eb5463b2fd3c4e392e5e0
SergioGMR <sergiogmr@icloud.com>
2024-10-17 21:02:21 +0100

    Added Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
+
   env: {
     schema: {
       TMDB_API_KEY: envField.string({ context: 'server', access: 'secret' })
     }
-  }
+  },
+
+  adapter: vercel()
 });
